f24729250b8a20476be98ead213fd05028b0cec7
wanjiku <annritawanjiku77@gmail.com>
2022-09-07 08:46:09 +0300

    completed assignment

--- main.js
+++ main.js
@@ -4,8 +4,34 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+const likeHearts = document.querySelectorAll('.like-glyph');
 
+let likePost = e => {
+  const heart = e.target;
+  mimicServerCall()
+  .then ( ()=> {
+    if (heart.innerText === EMPTY_HEART)
+    {
+      heart.innerText = FULL_HEART;
+      heart.className = "activated-heart"
+    }
+    else 
+    {
+      heart.innerText = EMPTY_HEART;
+      heart.className=""
+    }
+  })
+  .catch (error => {
+    const modal = document.getElementById('modal')
+    modal.className = ""
+    modal.innerText = error;
+    setTimeout(()=> modal.className = "hidden", 3000)
+  })
+}
 
+for (const glyph of likeHearts){
+  glyph.addEventListener("click", likePost)
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
@@ -23,3 +49,5 @@ function mimicServerCall(url="http://mimicServer.example.com", config={}) {
     }, 300);
   });
 }
+
+
